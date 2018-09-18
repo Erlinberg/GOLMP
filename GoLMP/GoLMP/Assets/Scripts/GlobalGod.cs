b0fca7425577e8774fcac41d82e97628ef9d4e12
Erlinberg <develierol@gmail.com>
2018-09-18 13:51:55 +0300

    оо

--- GoLMP/GoLMP/Assets/Scripts/GlobalGod.cs
+++ GoLMP/GoLMP/Assets/Scripts/GlobalGod.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class GlobalGod : MonoBehaviour
 {
@@ -32,6 +33,77 @@ public class GlobalGod : MonoBehaviour
 
     private float timeLeft;
 
+    private void Write(string text)
+    {
+        if (!Directory.Exists("Settings"))
+        {
+            Directory.CreateDirectory("Settings");
+        }
+
+
+        if (!File.Exists("Settings/settings.ini"))
+        {
+            File.Create("Settings/settings.ini");
+        }
+
+
+        File.AppendAllText("Settings/settings.ini", text);
+    }
+
+    private int x, y, z, id, new_x, new_y, new_z;
+
+    private int[] delta = new int[3];
+
+    private void Get_coords()
+    {
+        x = id % FieldSize;
+        y = (id / FieldSize) % FieldSize;
+        z = id / (FieldSize * FieldSize);
+    }
+    private void Get_id()
+   {
+        id = z * (FieldSize * FieldSize) + y * FieldSize + x;
+   }
+    private int In_field(int dx, int dy, int dz)
+    {
+        Get_coords();
+        new_x = x + dx;
+        new_y = y + dy;
+        new_z = z + dz;
+        if (dx == 0 & dy == 0 & dz == 0)
+        {
+            return 0;
+        }
+        else if (new_x < 0 || new_x > FieldSize - 1)
+            return 0;
+
+        else if (new_y < 0 || new_y> FieldSize - 1)
+            return 0;
+
+        else if (new_z < 0 || new_z > FieldSize - 1)
+            return 0;
+        else
+        {
+            return SellArrayCopy[id];
+        }
+    }
+    private void Sum_around()
+    {
+        Write("74");
+        sum = 0;
+        delta[0] = -1;
+        delta[1] = 0;
+        delta[2] = 1;
+        Write("79");
+        foreach (int dx in delta)
+            foreach (int dy in delta)
+                foreach (int dz in delta)
+                {
+                    Write("83");
+                    sum += In_field(dx, dy, dz);
+                    Write("86");
+                }
+    }
     private void CreateField()
     {
         for (int f = 0; f < FieldSize / 2; f++)
@@ -50,264 +122,270 @@ public class GlobalGod : MonoBehaviour
 
     private void RuleCnt()
     {
-        for (int id = 0; id < Mathf.Pow(FieldSize,3); id++)
+        Write("107");
+        for (int _id = 0; id < Mathf.Pow(FieldSize,3); id++)
         {
-            // Определение sum
+            Write("109");
+            id = _id;
+            Write("111");
+            Sum_around();
+            //// Определение sum
 
-            // 12
-            if (id - 1 > 0)
-            {
-                if (SellArray[id - 1] == 1)
-                {
-                    sum += 1;
-                }
-            }
+            //// 12
+            //if (id - 1 > 0)
+            //{
+            //    if (SellArray[id - 1] == 1)
+            //    {
+            //        sum += 1;
+            //    }
+            //}
             
-            // 14
-            if (id + 1 < Mathf.Pow(FieldSize, 3))
-            {
-                if (SellArray[id + 1] == 1)
-                {
-                    sum += 1;
-                }
-            }
+            //// 14
+            //if (id + 1 < Mathf.Pow(FieldSize, 3))
+            //{
+            //    if (SellArray[id + 1] == 1)
+            //    {
+            //        sum += 1;
+            //    }
+            //}
 
-            // 10
-            if (id - FieldSize > 0)
-            {
-                if (SellArray[id - FieldSize] == 1)
-                {
-                    sum += 1;
-                }
-            }
+            //// 10
+            //if (id - FieldSize > 0)
+            //{
+            //    if (SellArray[id - FieldSize] == 1)
+            //    {
+            //        sum += 1;
+            //    }
+            //}
 
-            // 16
-            if (id + FieldSize < Mathf.Pow(FieldSize, 3))
-            {
-                if (SellArray[id + FieldSize] == 1)
-                {
-                    sum += 1;
-                }
-            }
+            //// 16
+            //if (id + FieldSize < Mathf.Pow(FieldSize, 3))
+            //{
+            //    if (SellArray[id + FieldSize] == 1)
+            //    {
+            //        sum += 1;
+            //    }
+            //}
 
-            // 22
-            if (id + Mathf.Pow(FieldSize, 2) < Mathf.Pow(FieldSize, 3))
-            {
-                if (SellArray[id + Mathf.RoundToInt(Mathf.Pow(FieldSize, 2))] == 1)
-                {
-                    sum += 1;
-                }
-            }
+            //// 22
+            //if (id + Mathf.Pow(FieldSize, 2) < Mathf.Pow(FieldSize, 3))
+            //{
+            //    if (SellArray[id + Mathf.RoundToInt(Mathf.Pow(FieldSize, 2))] == 1)
+            //    {
+            //        sum += 1;
+            //    }
+            //}
 
-            // 4
-            if (id - Mathf.Pow(FieldSize, 2) > 0)
-            {
-                if (SellArray[id - Mathf.RoundToInt(Mathf.Pow(FieldSize, 2))] == 1)
-                {
-                    sum += 1;
-                }
-            }
+            //// 4
+            //if (id - Mathf.Pow(FieldSize, 2) > 0)
+            //{
+            //    if (SellArray[id - Mathf.RoundToInt(Mathf.Pow(FieldSize, 2))] == 1)
+            //    {
+            //        sum += 1;
+            //    }
+            //}
 
-            // 8
-            if (id - Mathf.Pow(FieldSize, 2) + FieldSize + 1 > 0)
-            {
-                if (SellArray[id - Mathf.RoundToInt(Mathf.Pow(FieldSize, 2)) + FieldSize + 1] == 1)
-                {
-                    sum += 1;
-                }
-            }
+            //// 8
+            //if (id - Mathf.Pow(FieldSize, 2) + FieldSize + 1 > 0)
+            //{
+            //    if (SellArray[id - Mathf.RoundToInt(Mathf.Pow(FieldSize, 2)) + FieldSize + 1] == 1)
+            //    {
+            //        sum += 1;
+            //    }
+            //}
 
-            // 6
-            if (id - Mathf.Pow(FieldSize, 2) + FieldSize - 1 > 0)
-            {
-                if (SellArray[id - Mathf.RoundToInt(Mathf.Pow(FieldSize, 2)) + FieldSize - 1] == 1)
-                {
-                    sum += 1;
-                }
-            }
+            //// 6
+            //if (id - Mathf.Pow(FieldSize, 2) + FieldSize - 1 > 0)
+            //{
+            //    if (SellArray[id - Mathf.RoundToInt(Mathf.Pow(FieldSize, 2)) + FieldSize - 1] == 1)
+            //    {
+            //        sum += 1;
+            //    }
+            //}
 
-            // 7
-            if (id - Mathf.Pow(FieldSize, 2) + FieldSize > 0)
-            {
-                if (SellArray[id - Mathf.RoundToInt(Mathf.Pow(FieldSize, 2)) + FieldSize] == 1)
-                {
-                    sum += 1;
-                }
-            }
+            //// 7
+            //if (id - Mathf.Pow(FieldSize, 2) + FieldSize > 0)
+            //{
+            //    if (SellArray[id - Mathf.RoundToInt(Mathf.Pow(FieldSize, 2)) + FieldSize] == 1)
+            //    {
+            //        sum += 1;
+            //    }
+            //}
 
-            // 5
-            if (id - Mathf.Pow(FieldSize, 2) + 1 > 0)
-            {
-                if (SellArray[id - Mathf.RoundToInt(Mathf.Pow(FieldSize, 2)) + 1] == 1)
-                {
-                    sum += 1;
-                }
-            }
+            //// 5
+            //if (id - Mathf.Pow(FieldSize, 2) + 1 > 0)
+            //{
+            //    if (SellArray[id - Mathf.RoundToInt(Mathf.Pow(FieldSize, 2)) + 1] == 1)
+            //    {
+            //        sum += 1;
+            //    }
+            //}
 
-            // 3 
-            if (id - Mathf.Pow(FieldSize, 2) - 1 > 0)
-            {
-                if (SellArray[id - Mathf.RoundToInt(Mathf.Pow(FieldSize, 2)) - 1] == 1)
-                {
-                    sum += 1;
-                }
-            }
+            //// 3 
+            //if (id - Mathf.Pow(FieldSize, 2) - 1 > 0)
+            //{
+            //    if (SellArray[id - Mathf.RoundToInt(Mathf.Pow(FieldSize, 2)) - 1] == 1)
+            //    {
+            //        sum += 1;
+            //    }
+            //}
 
-            // 2
-            if (id - Mathf.Pow(FieldSize, 2) - FieldSize + 1 > 0)
-            {
-                if (SellArray[id - Mathf.RoundToInt(Mathf.Pow(FieldSize, 2)) - FieldSize + 1] == 1)
-                {
-                    sum += 1;
-                }
-            }
+            //// 2
+            //if (id - Mathf.Pow(FieldSize, 2) - FieldSize + 1 > 0)
+            //{
+            //    if (SellArray[id - Mathf.RoundToInt(Mathf.Pow(FieldSize, 2)) - FieldSize + 1] == 1)
+            //    {
+            //        sum += 1;
+            //    }
+            //}
 
-            // 0
-            if (id - Mathf.Pow(FieldSize, 2) - FieldSize - 1 > 0)
-            {
-                if (SellArray[id - Mathf.RoundToInt(Mathf.Pow(FieldSize, 2)) - FieldSize - 1] == 1)
-                {
-                    sum += 1;
-                }
-            }
+            //// 0
+            //if (id - Mathf.Pow(FieldSize, 2) - FieldSize - 1 > 0)
+            //{
+            //    if (SellArray[id - Mathf.RoundToInt(Mathf.Pow(FieldSize, 2)) - FieldSize - 1] == 1)
+            //    {
+            //        sum += 1;
+            //    }
+            //}
 
-            // 1
-            if (id - Mathf.Pow(FieldSize, 2) - FieldSize > 0)
-            {
-                if (SellArray[id - Mathf.RoundToInt(Mathf.Pow(FieldSize, 2)) - FieldSize] == 1)
-                {
-                    sum += 1;
-                }
-            }
+            //// 1
+            //if (id - Mathf.Pow(FieldSize, 2) - FieldSize > 0)
+            //{
+            //    if (SellArray[id - Mathf.RoundToInt(Mathf.Pow(FieldSize, 2)) - FieldSize] == 1)
+            //    {
+            //        sum += 1;
+            //    }
+            //}
 
-            // 9
-            if (id - FieldSize - 1 > 0)
-            {
-                if (SellArray[id - FieldSize - 1] == 1)
-                {
-                    sum += 1;
-                }
-            }
+            //// 9
+            //if (id - FieldSize - 1 > 0)
+            //{
+            //    if (SellArray[id - FieldSize - 1] == 1)
+            //    {
+            //        sum += 1;
+            //    }
+            //}
 
-            // 11
-            if (id - FieldSize + 1 > 0)
-            {
-                if (SellArray[id - FieldSize + 1] == 1)
-                {
-                    sum += 1;
-                }
-            }
+            //// 11
+            //if (id - FieldSize + 1 > 0)
+            //{
+            //    if (SellArray[id - FieldSize + 1] == 1)
+            //    {
+            //        sum += 1;
+            //    }
+            //}
 
-            // 15
-            if (id + FieldSize - 1 < Mathf.Pow(FieldSize, 3))
-            {
-                if (SellArray[id + FieldSize - 1] == 1)
-                {
-                    sum += 1;
-                }
-            }
+            //// 15
+            //if (id + FieldSize - 1 < Mathf.Pow(FieldSize, 3))
+            //{
+            //    if (SellArray[id + FieldSize - 1] == 1)
+            //    {
+            //        sum += 1;
+            //    }
+            //}
 
-            // 17
-            if (id + FieldSize + 1 < Mathf.Pow(FieldSize, 3))
-            {
-                if (SellArray[id + FieldSize + 1] == 1)
-                {
-                    sum += 1;
-                }
-            }
+            //// 17
+            //if (id + FieldSize + 1 < Mathf.Pow(FieldSize, 3))
+            //{
+            //    if (SellArray[id + FieldSize + 1] == 1)
+            //    {
+            //        sum += 1;
+            //    }
+            //}
 
-            // 21
-            if (id + Mathf.Pow(FieldSize, 2) - 1 < Mathf.Pow(FieldSize, 3))
-            {
-                if (SellArray[id + Mathf.RoundToInt(Mathf.Pow(FieldSize, 2)) - 1] == 1)
-                {
-                    sum += 1;
-                }
-            }
+            //// 21
+            //if (id + Mathf.Pow(FieldSize, 2) - 1 < Mathf.Pow(FieldSize, 3))
+            //{
+            //    if (SellArray[id + Mathf.RoundToInt(Mathf.Pow(FieldSize, 2)) - 1] == 1)
+            //    {
+            //        sum += 1;
+            //    }
+            //}
 
-            // 23
-            if (id + Mathf.Pow(FieldSize, 2) + 1 < Mathf.Pow(FieldSize, 3))
-            {
-                if (SellArray[id + Mathf.RoundToInt(Mathf.Pow(FieldSize, 2)) + 1] == 1)
-                {
-                    sum += 1;
-                }
-            }
+            //// 23
+            //if (id + Mathf.Pow(FieldSize, 2) + 1 < Mathf.Pow(FieldSize, 3))
+            //{
+            //    if (SellArray[id + Mathf.RoundToInt(Mathf.Pow(FieldSize, 2)) + 1] == 1)
+            //    {
+            //        sum += 1;
+            //    }
+            //}
 
-            // 19
-            if (id + Mathf.Pow(FieldSize, 2) - FieldSize < Mathf.Pow(FieldSize, 3))
-            {
-                if (SellArray[id + Mathf.RoundToInt(Mathf.Pow(FieldSize, 2)) - FieldSize] == 1)
-                {
-                    sum += 1;
-                }
-            }
+            //// 19
+            //if (id + Mathf.Pow(FieldSize, 2) - FieldSize < Mathf.Pow(FieldSize, 3))
+            //{
+            //    if (SellArray[id + Mathf.RoundToInt(Mathf.Pow(FieldSize, 2)) - FieldSize] == 1)
+            //    {
+            //        sum += 1;
+            //    }
+            //}
 
-            // 18
-            if (id + Mathf.Pow(FieldSize, 2) - FieldSize - 1 < Mathf.Pow(FieldSize, 3))
-            {
-                if (SellArray[id + Mathf.RoundToInt(Mathf.Pow(FieldSize, 2)) - FieldSize - 1] == 1)
-                {
-                    sum += 1;
-                }
-            }
+            //// 18
+            //if (id + Mathf.Pow(FieldSize, 2) - FieldSize - 1 < Mathf.Pow(FieldSize, 3))
+            //{
+            //    if (SellArray[id + Mathf.RoundToInt(Mathf.Pow(FieldSize, 2)) - FieldSize - 1] == 1)
+            //    {
+            //        sum += 1;
+            //    }
+            //}
 
-            // 20
-            if (id + Mathf.Pow(FieldSize, 2) - FieldSize + 1 < Mathf.Pow(FieldSize, 3))
-            {
-                if (SellArray[id + Mathf.RoundToInt(Mathf.Pow(FieldSize, 2)) - FieldSize + 1] == 1)
-                {
-                    sum += 1;
-                }
-            }
+            //// 20
+            //if (id + Mathf.Pow(FieldSize, 2) - FieldSize + 1 < Mathf.Pow(FieldSize, 3))
+            //{
+            //    if (SellArray[id + Mathf.RoundToInt(Mathf.Pow(FieldSize, 2)) - FieldSize + 1] == 1)
+            //    {
+            //        sum += 1;
+            //    }
+            //}
 
-            // 24
-            if (id + Mathf.Pow(FieldSize, 2) + FieldSize - 1 < Mathf.Pow(FieldSize, 3))
-            {
-                if (SellArray[id + Mathf.RoundToInt(Mathf.Pow(FieldSize, 2)) + FieldSize - 1] == 1)
-                {
-                    sum += 1;
-                }
-            }
+            //// 24
+            //if (id + Mathf.Pow(FieldSize, 2) + FieldSize - 1 < Mathf.Pow(FieldSize, 3))
+            //{
+            //    if (SellArray[id + Mathf.RoundToInt(Mathf.Pow(FieldSize, 2)) + FieldSize - 1] == 1)
+            //    {
+            //        sum += 1;
+            //    }
+            //}
 
-            // 25
-            if (id + Mathf.Pow(FieldSize, 2) + FieldSize < Mathf.Pow(FieldSize, 3))
-            {
-                if (SellArray[id + Mathf.RoundToInt(Mathf.Pow(FieldSize, 2)) + FieldSize] == 1)
-                {
-                    sum += 1;
-                }
-            }
+            //// 25
+            //if (id + Mathf.Pow(FieldSize, 2) + FieldSize < Mathf.Pow(FieldSize, 3))
+            //{
+            //    if (SellArray[id + Mathf.RoundToInt(Mathf.Pow(FieldSize, 2)) + FieldSize] == 1)
+            //    {
+            //        sum += 1;
+            //    }
+            //}
 
-            // 26
-            if (id + Mathf.Pow(FieldSize, 2) + FieldSize + 1 < Mathf.Pow(FieldSize, 3))
-            {
-                if (SellArray[id + Mathf.RoundToInt(Mathf.Pow(FieldSize, 2)) + FieldSize + 1] == 1)
-                {
-                    sum += 1;
-                }
-            }
+            //// 26
+            //if (id + Mathf.Pow(FieldSize, 2) + FieldSize + 1 < Mathf.Pow(FieldSize, 3))
+            //{
+            //    if (SellArray[id + Mathf.RoundToInt(Mathf.Pow(FieldSize, 2)) + FieldSize + 1] == 1)
+            //    {
+            //        sum += 1;
+            //    }
+            //}
 
-            // Окончание определения sum
-            // Начало условий
+            //// Окончание определения sum
+            //// Начало условий
 
-            if (SellArray[id] == 1)
-            {
-                if (sum > Max_for_live || sum < Min_for_live)
-                {
-                    SellArrayCopy[id] = 0;
-                }
+            //if (SellArray[id] == 1)
+            //{
+            //    if (sum > Max_for_live || sum < Min_for_live)
+            //    {
+            //        SellArrayCopy[id] = 0;
+            //    }
 
-            }
-            else
-            {
-                if (sum == Optimal_for_birth)
-                {
-                    SellArrayCopy[id] = 1;
-                }
-            }
-            sum = 0;
+            //}
+            //else
+            //{
+            //    if (sum == Optimal_for_birth)
+            //    {
+            //        SellArrayCopy[id] = 1;
+            //    }
+            //}
+            //sum = 0;
         }
+
         if (!(SellArrayCopy == SellArray))
         {
             System.Array.Copy(SellArrayCopy,SellArray,FieldSize*FieldSize*FieldSize);
