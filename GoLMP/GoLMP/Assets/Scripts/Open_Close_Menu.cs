ea71924cd6d190b08298cf2d75aa3d38b8846ae2
Erlinberg <develierol@gmail.com>
2018-09-26 15:25:46 +0300

    Commit

---
+++ GoLMP/GoLMP/Assets/Scripts/Open_Close_Menu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Open_Close_Menu : MonoBehaviour {
+
+    public GameObject Menu;
+
+    
+
+    public void MenuControll()
+    {
+        Menu.SetActive((Menu.activeSelf == false));
+    }
+}
